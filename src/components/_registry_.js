65e1669a8c349db3799e75f1e8239da2e04be837
Joe Fleming <joe.fleming@gmail.com>
2015-01-23 23:53:16 -0700

    move nav to a site-nav element

--- src/components/_registry_.js
+++ src/components/_registry_.js
@@ -3,3 +3,5 @@ import CustomElement from 'lib/CustomElement'
 export const GithubApiTest = new CustomElement('github-api-test', require('GithubApiTest'));
 export const IssueList = new CustomElement('enhance-issue-list', require('IssueList'));
 export const PayloadCreate = new CustomElement('payload-create', require('PayloadCreate'));
+
+export const SiteNav = new CustomElement('site-nav', require('templates/siteNav.html'));
