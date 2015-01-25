45578d987692eaf92129552e1326d1677f126845
Jason Walsh <Jason.Walsh@colonyamerican.com>
2015-01-25 11:38:53 -0700

    run unique counts on upvotes and downvotes

--- src/components/IssueView.js
+++ src/components/IssueView.js
@@ -19,6 +19,8 @@ export default component({
       route: null,
       issue: null,
       comments: null,
+      upvotes : [],
+      downvotes : [],
       user: UserStore.user,
       newComment: '' // new user comment from textarea
     };
@@ -30,8 +32,36 @@ export default component({
     this.listenTo(CommentActions.downvoteSuccess, this.rerenderView)
     this.listenTo(RequestStore, _.noop, this.routeUpdated)
     this.bindTo(IssueStore, 'issue')
-    this.bindTo(CommentStore, 'comments')
+    this.listenTo(CommentStore, this.updateComments)
     log.msg('@TODO load issues -- IssueList.js')
+  },
+
+  updateComments(comments) {
+    var votes = {};
+    var upvotePattern = /\+1/;
+    var downvotePattern = /\-1/;
+
+    _.each(comments, (comment) => {
+      if(upvotePattern.test(comment.body)) {
+        votes[comment.user.login] = { type : 'upvote', comment: comment };
+      } else if (downvotePattern.test(comment.body)) {
+        votes[comment.user.login] = { type: 'downvote', comment: comment };
+      }
+    });
+
+    var upvotes = _.filter(votes, (vote) => {
+      if (_.isEqual(vote.type, 'upvote')) {
+        return vote.comment;
+      }
+    });
+
+    var downvotes = _.filter(votes, (vote) => {
+      if (_.isEqual(vote.type, 'downvote')) {
+        return vote.comment;
+      }
+    });
+
+    this.setState({comments: comments, upvotes : _.pluck(upvotes, 'comment'), downvotes: _.pluck(downvotes, 'comment') })
   },
 
   routeUpdated(route) {
@@ -45,12 +75,34 @@ export default component({
     CommentActions.getByIssue(issueNumber)
   },
 
-  _renderFacepile() {
 
+  _renderFaces(dom, votes) {
+    var { a, img } = dom;
+    return _.map(votes, (vote) => {
+      return a(
+          img({
+            alt: vote.user.login,
+            title : vote.user.login,
+            class: 'profile-image',
+            src: 'https://avatars3.githubusercontent.com/u/' + vote.user.id + '?v=3&amp;s=80'
+          })
+        )
+    })
   },
 
-  _renderUserComment() {
 
+  _renderFacepile(dom, type, state) {
+    var { div, h5 } = dom;
+    var className = type === 'upvotes' ? 'success' : 'danger';
+    var typeOfUser = type === 'upvotes' ? '+1`ers' : '-1`ers';
+    var votes = state[type];
+
+    return div({class: 'participants-wrapper'},
+      h5({class: 'bold italic text-' + className}, typeOfUser),
+      div({class: 'facepile-container'},
+        this._renderFaces(dom, votes)
+      )
+    );
   },
 
   renderComments(dom, comments) {
@@ -130,8 +182,8 @@ export default component({
   },
 
   render(props, state) {
-    var {div, ul, li, span, h1, h3, h4, h5, a, img, i, small, p} = this.dom
-    var { issue, comments, user } = state
+    var {div, ul, li, span, h1, h3, h4, a, img, i, small, p} = this.dom
+    var { issue, comments, user, upvotes, downvotes } = state
 
     // deps
     if (! issue) {
@@ -151,19 +203,19 @@ export default component({
               )
             ),
             div({class:'vote-box'},
-              div({class: 'net-vote bold text-success'}, '100'),
+              div({class: 'net-vote bold text-success'}, upvotes.length - downvotes.length),
               div({class: 'vote-summary-wrapper'},
                 span({class: 'text-success float-left'},
                   small(
                     i({class: 'fa fa-plus'})
                   ),
-                  span({class: 'bold'}, '100')
+                  span({class: 'bold'}, upvotes.length)
                 ),
-                span({class: 'text-success float-danger'},
+                span({class: 'text-danger float-right'},
                   small(
                     i({class: 'fa fa-minus'})
                   ),
-                  span({class: 'bold'}, '100')
+                  span({class: 'bold'}, downvotes.length)
                 )
               )
             ),
@@ -176,45 +228,10 @@ export default component({
               ),
               h3({class: 'italic text-gl'}, '#' + issue.number)
             )
-          ),
-          div({class: 'participant-wrapper'},
-            h5({class: 'bold italic text-success'}, '+1`ers'),
-            div({class: 'facepile-container'},
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/6923044?v=3&amp;s=80'})
-              ),
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/471821?v=3&amp;s=80'})
-              ),
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/322834?v=3&amp;s=80'})
-              ),
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/6923044?v=3&amp;s=80'})
-              ),
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/6923044?v=3&amp;s=80'})
-              ),
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/6923044?v=3&amp;s=80'})
-              ),
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/6923044?v=3&amp;s=80'})
-              )
-            )
-          ),
-          div({class: 'participants-wrapper'},
-            h5({class: 'bold italic text-danger'}, '-1`ers'),
-            div({class: 'facepile-container'},
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/6923044?v=3&amp;s=80'})
-              ),
-              a(
-                img({alt: 'elvarb', class: 'profile-image', src: 'https://avatars3.githubusercontent.com/u/471821?v=3&amp;s=80'})
-              )
-            )
           )
         ),
+        this._renderFacepile(this.dom, 'upvotes', state),
+        this._renderFacepile(this.dom, 'downvotes', state),
         div({class: 'description-wrapper'},
           p({class: 'lead text-gl'}, issue.body)
         ),
