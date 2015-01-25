22ce1870ff4ecfe01836822cd7c7a84e1561e68a
Jason Walsh <Jason.Walsh@colonyamerican.com>
2015-01-25 00:55:18 -0700

    issue list

--- src/components/_registry_.js
+++ src/components/_registry_.js
@@ -6,6 +6,7 @@ CustomElement.register('print-request', require('PrintRequest'));
 CustomElement.register('logging-test', require('LoggingTest'));
 CustomElement.register('user-badge', require('UserBadge'));
 CustomElement.register('issue-list', require('IssueList'));
+CustomElement.register('issue-view', require('IssueView'));
 CustomElement.register('site-nav', require('SiteNav'));
 
 CustomElement.register('site-devnav', require('siteNavDev.html'));
