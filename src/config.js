203b4f36e43379051785a6320fef5008591ed689
Spencer Alger <spencer@spenceralger.com>
2015-01-25 16:48:19 -0700

    update repo

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ export default {
   github: {
     baseUrl : 'https://www.github.com',
     apiUrl: 'https://api.github.com',
-    org: 'elasticsearch',
-    repo: 'kibana',
+    org: 'staticshowdown',
+    repo: 'ss15-enhance',
     enhanceLabel: 'enhancement'
   },
   storage: {
