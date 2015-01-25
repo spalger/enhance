ab8aa3780b436cd157a4252e2786deb886697bdf
Spencer Alger <spencer@spenceralger.com>
2015-01-24 21:00:01 -0700

    cleanup after detach, allowing for complete re-rendering

--- src/lib/CustomElement.js
+++ src/lib/CustomElement.js
@@ -13,16 +13,24 @@ export default class View {
         return this.renderAsHtml(content);
       }
 
-      throw new Error('content must be a string or an object with a render method');
+      throw new Error('content must be a string or a function with a render method');
     }
 
     proto.detachedCallback = function () {
+      this.cleanup();
+    };
+
+    proto.cleanup = function () {
       if (this.dekuTree) {
         this.dekuTree.remove();
       }
+
+      this.innerHTML = '';
     };
 
     proto.renderAsComponent = function (Component) {
+      this.cleanup();
+
       var attrsDesc = _.toArray(this.attributes);
       var attrs = _.transform(attrsDesc, function (attrs, attr) {
         attrs[attr.name] = attr.value;
@@ -32,6 +40,8 @@ export default class View {
     }
 
     proto.renderAsHtml = function (html) {
+      this.cleanup();
+
       this.innerHTML = html;
     }
 
