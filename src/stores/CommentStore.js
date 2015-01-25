36e8a81a48fa21a0955e24a6fe889a8406f76317
Jason Walsh <Jason.Walsh@colonyamerican.com>
2015-01-24 18:35:32 -0700

    update comment store to use github abstraction

--- src/stores/CommentStore.js
+++ src/stores/CommentStore.js
@@ -1,11 +1,11 @@
 import Reflux from 'reflux'
-import request from 'superagent'
 import config from 'config'
+import log from 'lib/log'
+import github from 'lib/github'
 
 import CommentActions from 'actions/CommentActions'
-import UserStore from 'stores/UserStore'
 
-var { apiUrl, author, repo, enhanceLabel } = config.github;
+var { author, repo, enhanceLabel } = config.github;
 const UPVOTE = ':+1:'
 const DOWNVOTE = ':-1:'
 
@@ -13,21 +13,14 @@ export default Reflux.createStore({
   listenables: CommentActions,
 
   _create(issueNumber, comment) {
-    var token = UserStore.getGithubToken()
-
-    request
-      .post([ apiUrl, 'repos', author, repo, 'issues', issueNumber, 'comments' ] .join('/'))
-      .send({ body : comment })
-      .set('Authorization', 'token ' + token) // required token
-      .end(function(error, res) {
-        if (error) {
-          console.log('Error creating a comment: ' + error);
-        }
-
-        if (res) {
-          console.log(res); // @todo handle response
-        }
-      });
+    github
+      .method('post')
+      .path([ 'repos', author, repo, 'issues', issueNumber, 'comments' ])
+      .body({ body : comment })
+      .send()
+      .then((response) => {
+        log.msg(response.body);
+      })
   },
 
   /* sample return object keys: url, html_url, issue_url, id, user, created_at, updated_at, body */
@@ -43,24 +36,13 @@ export default Reflux.createStore({
       payload.since = since
     }
 
-    request
-      .get([ apiUrl, 'repos', author, repo, 'issues', issueNumber, 'comments' ] .join('/'))
-      //.send({ sort : 'updated', direction : 'desc', since : '2015-01-01 })
-      //.set('Authorization', 'foobar')
-      .end(function(error, res) {
-        if (error) {
-          console.log('Error getting all repo comments: ' + error);
-        }
-
-        if (res && res.text) {
-          try {
-            var comments = JSON.parse(res.text);
-            console.log(comments);
-          } catch (err) {
-            console.log('Error parsing JSON while getting all repo comments');
-          }
-        }
-      });
+    github
+      .method('get')
+      .path([ 'repos', author, repo, 'issues', issueNumber, 'comments' ])
+      .send()
+      .then((comments) => {
+        log.msg(comments);
+      })
   },
 
   onUpvote(issueNumber) {
