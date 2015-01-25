4c90da7c8b8cb247eac6ff848989adae6d2317f3
Spencer Alger <spencer@spenceralger.com>
2015-01-24 21:18:12 -0700

    save

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
+import _ from 'lodash'
 import domready from 'domready'
 
 import 'components/_registry_'
 import 'pages/_registry_'
 
+import CustomElement from 'lib/CustomElement'
+import UserActions from 'actions/UserActions'
+import {ListenerMethods} from 'reflux'
 import router from 'lib/router'
-domready(router.start)
+
+class EnhanceApp extends CustomElement {
+  attachedCallback() {
+    domready(router.start)
+
+    UserActions.listenTo()
+  }
+}
+
+_.assign(EnhanceApp.prototype, ListenerMethods);
+
+document.createElement('enhance-app', {
+  prototype: Object.create(EnhanceApp.prototype)
+})
