9781da4f1aa0c924bc5839b5122f633a30c7cd8e
Spencer Alger <spencer@spenceralger.com>
2015-01-25 16:48:57 -0700

    update tag

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export default {
     apiUrl: 'https://api.github.com',
     org: 'staticshowdown',
     repo: 'ss15-enhance',
-    enhanceLabel: 'enhancement'
+    enhanceLabel: 'priority'
   },
   storage: {
     index: 'enhance-store'
