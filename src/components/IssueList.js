d54f62a238bf3d50f29e0c84d73f221bfb41aee5
Jason Walsh <Jason.Walsh@colonyamerican.com>
2015-01-24 22:42:06 -0700

    issue-list component added

--- src/components/IssueList.js
+++ src/components/IssueList.js
@@ -17,15 +17,52 @@ export default component({
     // deps
     if (!state.route) return;
 
-    var {div, h1, ul, li} = this.dom
+    var {div, ul, li, a, img, i, p, span, h3} = this.dom
 
-    return div(
-      h1('Issue #' + state.route.params.id),
-      ul(
-        li('Issue #1'),
-        li('Issue #2'),
-        li('Issue #3'),
-        li('Issue #4')
+    return div({class: 'panel panel-default'},
+      ul({class: 'list-group'},
+        li({class: 'list-group-item issue-list-item'},
+          div({class: 'col-xs-2 centered shrink-gutter-left shrink-gutter-right'},
+            div(
+              a({class: 'voting-links centered noselect'},
+                i({class: 'fa fa-caret-up'})
+              )
+            ),
+            div(
+              a({class: 'voting-links centered noselect'},
+                i({class: 'fa fa-caret-down'})
+              )
+            ),
+            p({class: 'big-count noselect'}, '100')
+          ),
+          div({class: 'col-xs-6 table-centered'},
+            h3({class: 'issue-name'}, 'Issue Name Enhancement'),
+            p({class: 'enhancement-description noselect'}, 'Turkey sirloin ground round hamburger. Aliquip in enim tail dolore salami')
+          ),
+          div({class: 'col-xs-3 no-gutter-left srhink-gutter-right'},
+            ul({class: 'list-unstyled issue-info-list noselect'},
+              li(
+                span({class: 'bold'}, '#120'),
+                ' opened ',
+                span({class: 'bold'}, '[time]'),
+                ' ago'
+              ),
+              li(
+                'by ',
+                a({class: 'bold'}, 'captainhurst')
+              ),
+              li({class: 'list-space'},
+                span({class: 'bold'}, '15'),
+                ' comments'
+              )
+            )
+          ),
+          div({class: 'col-xs-1 no-gutter'},
+            div({class: 'issue-profile-image'},
+              img({ src: 'https://avatars2.githubusercontent.com/u/4029749?v=3&s=50'})
+            )
+          )
+        )
       )
     )
   }
