4ef4ae36f5e026f3763064a7b41b8b6613d85e74
Joe Fleming <joe.fleming@gmail.com>
2015-01-31 17:55:53 -0700

    start butchering the issue details component

--- src/components/IssueView.js
+++ src/components/IssueView.js
@@ -1,23 +1,22 @@
-import component from 'lib/component'
 import _ from 'lodash'
 import deku from 'deku'
-
-import IssueStore from 'stores/IssueStore'
-import CommentStore from 'stores/CommentStore'
-import UserStore from 'stores/UserStore'
-import RequestStore from 'stores/RequestStore'
-import router from 'lib/router'
-
-import deku from 'deku'
-import LoadingContent from 'LoadingContent'
-
-import IssueActions from 'actions/IssueActions'
-import CommentActions from 'actions/CommentActions'
-
 import Markdown from 'Markdown'
 
+import component from 'lib/component'
+import router from 'lib/router'
 import log from 'lib/log'
 import uppercase from 'lib/uppercase'
+
+import VoteControls from 'components/VoteControls'
+import VoteFacepile from 'components/VoteFacepile'
+
+import RequestStore from 'stores/RequestStore'
+import LoadingContent from 'LoadingContent'
+import UserStore from 'stores/UserStore'
+import IssueStore from 'stores/IssueStore'
+import issueModel from 'models/issueModel'
+import CommentActions from 'actions/CommentActions'
+import CommentStore from 'stores/CommentStore'
 
 export default component({
   initialState() {
@@ -33,87 +32,30 @@ export default component({
     };
   },
 
-  afterMount() {
+  beforeMount() {
     this.listenTo(CommentActions.commentAddSuccess, this.rerenderView)
     this.listenTo(CommentActions.upvoteSuccess, this.rerenderView)
     this.listenTo(CommentActions.downvoteSuccess, this.rerenderView)
-    this.listenTo(IssueActions.fetchByIdFailed, router.goto.bind(null, '/'))
+
+    // this.listenTo(IssueActions.fetchByIdFailed, router.goto.bind(null, '/'))
+    this.listenTo(IssueStore, function(issues) {
+      var issue = issues.filter
+    })
+
     this.listenTo(RequestStore, _.noop, this.routeUpdated)
     this.bindTo(IssueStore, 'issue')
     this.listenTo(CommentStore, this.updateComments)
   },
 
-  updateComments(comments) {
-    var votes = {};
-    var upvotePattern = /\+1/;
-    var downvotePattern = /\-1/;
-
-    _.each(comments, (comment) => {
-      if(upvotePattern.test(comment.body)) {
-        votes[comment.user.login] = { type : 'upvote', comment: comment };
-      } else if (downvotePattern.test(comment.body)) {
-        votes[comment.user.login] = { type: 'downvote', comment: comment };
-      }
-    });
-
-    var upvotes = _.filter(votes, (vote) => {
-      if (_.isEqual(vote.type, 'upvote')) {
-        return vote.comment;
-      }
-    });
-
-    var downvotes = _.filter(votes, (vote) => {
-      if (_.isEqual(vote.type, 'downvote')) {
-        return vote.comment;
-      }
-    });
-
-    this.setState({comments: comments, upvotes : _.pluck(upvotes, 'comment'), downvotes: _.pluck(downvotes, 'comment') })
-  },
-
   routeUpdated(route) {
-    IssueActions.fetchById(route.params.id)
-    CommentActions.getByIssue(route.params.id)
+    // IssueActions.fetchById(route.params.id)
+    // CommentActions.getByIssue(route.params.id)
   },
 
   // comment just posted by user
   rerenderView(issueNumber) {
     this.setState({ newComment: '', comments: null })
     CommentActions.getByIssue(issueNumber)
-  },
-
-
-  _renderFaces(dom, votes) {
-    var { a, img } = dom;
-    return _.map(votes, (vote) => {
-      return a(
-          img({
-            alt: vote.user.login,
-            title : vote.user.login,
-            class: 'profile-image',
-            src: 'https://avatars3.githubusercontent.com/u/' + vote.user.id + '?v=3&amp;s=80'
-          })
-        )
-    })
-  },
-
-
-  _renderFacepile(dom, type, state) {
-    var { div, h5 } = dom;
-    var className = type === 'upvotes' ? 'success' : 'danger';
-    var typeOfUser = type === 'upvotes' ? '+1`ers' : '-1`ers';
-    var votes = state[type];
-
-    if (! votes.length) {
-      return div('')
-    }
-
-    return div({class: 'participants-wrapper'},
-      h5({class: 'bold italic text-' + className}, typeOfUser),
-      div({class: 'facepile-container'},
-        this._renderFaces(dom, votes)
-      )
-    );
   },
 
   renderComments(dom, comments) {
@@ -197,44 +139,6 @@ export default component({
     )
   },
 
-  renderArrows(dom, state) {
-    var { div, a, i } = dom
-    var { user, upvotes, downvotes } = state
-
-    var upvoteClass
-    var upvoteClickHandler = this.upvote.bind(null, state)
-    var downvoteClass
-    var downvoteClickHandler = this.downvote.bind(null, state)
-
-    if (user) {
-      // if user logged in, look for his vote
-      _.each(upvotes, (upvote) => {
-        if(_.isEqual(upvote.user.login, user.github.username)) {
-          upvoteClass = 'text-success';
-          upvoteClickHandler = _.noop;
-        }
-      });
-
-      if (!upvoteClass) {
-        _.each(downvotes, (downvote) => {
-          if(_.isEqual(downvote.user.login, user.github.username)) {
-            downvoteClass = 'text-danger';
-            downvoteClickHandler = _.noop;
-          }
-        })
-      }
-    }
-
-    return div({class: 'arrow-box'},
-      a({class: 'big-vote text-g'},
-        i({class: 'fa fa-caret-up ' + upvoteClass, onClick: upvoteClickHandler})
-      ),
-      a({class: 'big-vote down text-g'},
-        i({class: 'fa fa-caret-down ' + downvoteClass, onClick: downvoteClickHandler})
-      )
-    )
-  },
-
   toggleCommentBox(state) {
     this.setState({ showCommentBox : ! state.showCommentBox })
   },
@@ -258,7 +162,7 @@ export default component({
       div({class: 'row'},
         div({class: 'col-xs-12 col-sm-12 col-lg-10 col-lg-offset-1'},
           div({class: 'issue-header-wrapper'},
-            this.renderArrows(this.dom, state),
+            deku.dom(VoteControls, { issue: this.state.issue })
             div({class:'vote-box'},
               div({class: 'net-vote bold text-success'}, upvotes.length - downvotes.length),
               div({class: 'vote-summary-wrapper'},
@@ -290,8 +194,7 @@ export default component({
               )
             )
           ),
-        this._renderFacepile(this.dom, 'upvotes', state),
-        this._renderFacepile(this.dom, 'downvotes', state),
+        deku.dom(VoteFacepile, { issue: this.state.issue }),
         div({class: 'description-wrapper'},
           p({class: 'lead text-gl'},
             deku.dom(Markdown, { markdown: issue.body, style: 'description' })
